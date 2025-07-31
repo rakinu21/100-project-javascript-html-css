394d848f002e2c29809cd865e410a0f14bc4ad61
rakinu21 <ralphkinethugdamin@gmail.com>
2025-07-31 13:25:50 +0800

    animated search bar is done

---
+++ 66. animated search bar/script.js
@@ -0,0 +1,8 @@
+const search = document.querySelector('.search');
+
+const searhInput = document.querySelector('.searchInput');
+
+
+search.addEventListener('click',()=>{
+    searhInput.classList.toggle('active')
+})
